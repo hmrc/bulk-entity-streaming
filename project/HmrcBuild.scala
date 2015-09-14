76d682e525e3d97f6e3e07088a1e44e66f0d18db
Duncan Crawford <duncan@noughtsandones.co.uk>
2015-09-14 17:15:31 +0100

    add hmrc bintray resolver
    Additionally refactor the build to conform to other builds

--- project/HmrcBuild.scala
+++ project/HmrcBuild.scala
@@ -1,49 +1,49 @@
-import play.core.PlayVersion
 import sbt.Keys._
 import sbt._
-import uk.gov.hmrc.SbtAutoBuildPlugin
 import uk.gov.hmrc.versioning.SbtGitVersioning
-
 
 object HmrcBuild extends Build {
 
-  import BuildDependencies._
   import uk.gov.hmrc.DefaultBuildSettings._
+  import uk.gov.hmrc._
 
-  val nameApp = "bulk-entity-streaming"
+  val appName = "bulk-entity-streaming"
 
-  lazy val BulkEntityStreaming = (project in file("."))
+  lazy val bulkEntityStreaming = Project(appName, file("."))
     .enablePlugins(SbtAutoBuildPlugin, SbtGitVersioning)
     .settings(
       scalaVersion := "2.11.7",
-      libraryDependencies ++= Seq(
-        Compile.httpVerbs,
-        Test.scalaTest,
-        Test.pegdown,
-        Test.playTest
-      ),
-      Developers()
+      libraryDependencies ++= AppDependencies(),
+      crossScalaVersions := Seq("2.11.7"),
+      resolvers := Seq(
+        Resolver.bintrayRepo("hmrc", "releases"),
+        Resolver.typesafeRepo("releases")
+      )
     )
 }
 
-private object BuildDependencies {
+private object AppDependencies {
 
-  val httpVerbsVersion = "1.10.0"
-  object Compile {
-    val httpVerbs = "uk.gov.hmrc" %% "http-verbs" % httpVerbsVersion
+  import play.core.PlayVersion
+
+  val compile = Seq(
+    "uk.gov.hmrc" %% "http-verbs" % "1.10.0" % "provided"
+  )
+
+  trait TestDependencies {
+    lazy val scope: String = "test"
+    lazy val test: Seq[ModuleID] = ???
   }
 
-  sealed abstract class Test(scope: String) {
-    val scalaTest = "org.scalatest" %% "scalatest" % "2.2.4" % scope
-    val pegdown = "org.pegdown" % "pegdown" % "1.5.0" % scope
-    val playTest = "com.typesafe.play" %% "play-test" % PlayVersion.current % scope
+  object Test {
+    def apply() = new TestDependencies {
+      override lazy val test = Seq(
+        "org.scalatest" %% "scalatest" % "2.2.4" % scope,
+        "org.pegdown" % "pegdown" % "1.5.0" % scope,
+        "com.typesafe.play" %% "play-test" % PlayVersion.current % scope
+      )
+    }.test
   }
 
-  object Test extends Test("test")
-
-}
-
-object Developers {
-
-  def apply() = developers := List[Developer]()
+  def apply() = compile ++ Test()
 }
